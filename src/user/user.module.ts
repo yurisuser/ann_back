180412986710e3dc517a41fbcba9ce8de11c951d
yuri.suser <yuri.suser@gmail.com>
2019-10-17 19:14:08 +0300

    03.01_roles_add_default_roles

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,13 +2,14 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from './user.entity';
+import { Role } from './role.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([User]),
+        TypeOrmModule.forFeature([User, Role]),
         forwardRef(() => AuthModule),
     ],
     providers: [UserService],
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,33 +4,41 @@ import { Repository } from 'typeorm';
 
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create.user.dto';
+import { Role } from './role.entity';
 
 @Injectable()
 export class UserService {
     readonly relation = ['role'];
     constructor(
         @InjectRepository(User)
-        private repository: Repository<User>,
+        private repositoryUser: Repository<User>,
+        @InjectRepository(Role)
+        private repositoryRole: Repository<Role>,
     ) {}
 
     async findAll(): Promise<User[]> {
-       return await this.repository.find({relations: this.relation});
+       return await this.repositoryUser.find({relations: this.relation});
     }
 
     async findByName(name: string): Promise<User> {
-        return await this.repository.findOne({name}, {relations: this.relation});
+        return await this.repositoryUser.findOne({name}, {relations: this.relation});
     }
 
     async findById(id: number): Promise<User> {
-        return await this.repository.findOne({id}, {relations: this.relation});
+        return await this.repositoryUser.findOne({id}, {relations: this.relation});
     }
 
     async create(user: CreateUserDto): Promise<User> {
-        const us = await this.repository.save({name: user.name, password: user.password});
-        return us;
+        const role = await this.getRoleDefault();
+        const userNew = await this.repositoryUser.save({name: user.name, password: user.password, role: {id: role.id}});
+        return userNew;
     }
 
     async delete(id: number) {
-        return this.repository.delete(id);
+        return this.repositoryUser.delete(id);
+    }
+
+    private async getRoleDefault() {
+        return  await this.repositoryRole.findOne({role: 'default'}) || this.repositoryRole.save({role: 'default'});
     }
 }
